834cc85936960635cba9f8a5446a8f75150ffe4c
Maysam <maysamalfaddagh@gmail.com>
2020-01-24 17:22:53 +0300

    Adding the classification model

--- Disaster Response Pipeline Project/models/train_classifier.py
+++ Disaster Response Pipeline Project/models/train_classifier.py
@@ -1,24 +1,111 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sqlalchemy import create_engine
+
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+import pickle
 
 def load_data(database_filepath):
-    pass
+    '''
+    Imports data to the workspace
+    INPUT 
+        databaseـfilepath - (str) Filepath for csv file containing dataset.    
+    OUTPUT
+        X - (dataframe) contains  the features of the dataset.
+        Y - (dataframe) contains  the target of the dataset.
+        categories - (str) contains a list of all the categories names
+    '''
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('DisasterResponse', engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    categories = Y.columns
+    return X, Y, categories
 
 
 def tokenize(text):
-    pass
+    '''
+    INPUT 
+        text - Raw text to be processed 
+    OUTPUT
+        Returns a processed text that is lower case, tokenized, stripped, and lemmatized
+    '''
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+    Builds ML model pipeline
+    OUTPUT
+        CV - (GridSearch Result) a ML model
+    '''
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ])
+    
+    parameters = {'vect__max_df': (0.5,0.75),
+                 'clf__estimator__max_depth': [10, 50],
+                 'clf__estimator__min_samples_split':[2, 5, 10]}
+
+    cv = GridSearchCV(pipeline, parameters)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    Evaluate the model in respect to the test data set
+    INPUT 
+        model - ML model
+        X_test - Test data set of X
+        Y_test - Test data set of Y
+        category_names - (str list) Name of categories
+    '''
+    y_pred = model.predict(X_test)
+    
+    for i in range(len(category_names)):
+        category = category_names[i]
+        print(category)
+        print(classification_report(Y_test[category], y_pred[:, i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Saves the developed model to a specific file path
+    INPUT 
+        model - ML model
+        model_filepath - (str) Filepath for model to be saved at.
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
